69e8fb5d5635d4cafe55117e910e5d6032cdcc4a
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-05-26 17:59:03 -0600

    reed and create ready pana

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
-import {Text} from 'react-native';
-import {PaperProvider} from 'react-native-paper';
-import React, {useEffect, useMemo, useState} from 'react';
+import { Text } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import Auth from './src/screens/Auth';
 import AuthContext from "./src/components/context/AuthContext";
-import {setTokenApi} from "./src/api/token";
+import { setTokenApi } from "./src/api/token";
+import Dashboard from "./src/components/Home/DashBoard";
 
 export default function App() {
-
     const [auth, setAuth] = useState(undefined);
 
     useEffect(() => {
-        setAuth(null)
+        console.log("useEffect - setting auth to null");
+        setAuth(null);
     }, []);
 
-    const login = (user)=>{
-        /*console.log('LOGGIN APP')
-        console.log(user)*/
-        setTokenApi(user.jwt)
+    const login = (user) => {
+        console.log('LOGGING IN');
+        console.log(user);
+        setTokenApi(user.jwt);
         setAuth({
-            token : user.jwt,
-            idUser:user.id
-        })
+            token: user.jwt,
+            idUser: user.id
+        });
     }
 
     const authData = useMemo(() => ({
@@ -32,10 +33,13 @@ export default function App() {
 
     if (auth === undefined) return null;
 
+    console.log("Rendering App with auth:", auth);
+
     return (
         <AuthContext.Provider value={authData}>
             <PaperProvider>
-                {auth ? <Text>Zona de Usuarios</Text> : <Auth/>}
+                {auth ?
+                    <Dashboard /> : <Auth />}
             </PaperProvider>
         </AuthContext.Provider>
     );
