8473fe65d8764a8ce57d7ed415ba1448590cc08c
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-06-04 00:15:19 -0600

    compose y nueva url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-//export const API_URL = "http://192.168.1.78:1337"
+//export const API_URL = "http://172.26.48.44:1337"
 //export const API_URL = "https://appstrappi.azurewebsites.net"
-export const API_URL = "https://6b12-2806-104e-16-b14-8550-9a26-9b9e-af53.ngrok-free.app"
+export const API_URL = "https://a274-2806-104e-16-b14-3a63-bbff-fea8-3aac.ngrok-free.app"
 export const TOKEN = "token"
