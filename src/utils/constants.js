2e1412cc0b76324d691957cb0976c278b8ad0e10
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-06-04 22:22:01 -0600

    corrijo conflicto con reactivar maquina virtual

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 //export const API_URL = "http://172.26.48.44:1337"
 //export const API_URL = "https://appstrappi.azurewebsites.net"
-export const API_URL = "https://a274-2806-104e-16-b14-3a63-bbff-fea8-3aac.ngrok-free.app"
+//export const API_URL = "https://a274-2806-104e-16-b14-3a63-bbff-fea8-3aac.ngrok-free.app"
+export const API_URL = "http://strappi.eastus2.cloudapp.azure.com"
 export const TOKEN = "token"
