5515b93cf767554f8696aee2a24d1f588f666939
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-06-07 23:59:07 -0600

    corrijo constant.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://192.168.1.81:1337"
+//export const API_URL = "http://192.168.1.81:1337"
 //export const API_URL = "https://appstrappi.azurewebsites.net"
 //export const API_URL = "https://a274-2806-104e-16-b14-3a63-bbff-fea8-3aac.ngrok-free.app"
-//export const API_URL = "http://strappi.eastus2.cloudapp.azure.com"
+export const API_URL = "http://strappi.eastus2.cloudapp.azure.com"
 export const TOKEN = "token"
